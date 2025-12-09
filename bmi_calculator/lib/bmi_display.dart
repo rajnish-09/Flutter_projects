3fb75513347515e9b52bd15ecc57b7a7ea3aa472
rajnish-09 <rajnish9861@gmail.com>
2025-12-09 16:19:10 +0545

    make bmi display page

---
+++ bmi_calculator/lib/bmi_display.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class BmiDisplay extends StatefulWidget {
+  final double bmiValue;
+  const BmiDisplay({super.key, required this.bmiValue});
+
+  @override
+  State<BmiDisplay> createState() => _BmiDisplayState();
+}
+
+class _BmiDisplayState extends State<BmiDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 5, 22, 39),
+      appBar: AppBar(
+        title: Text(
+          "Result",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 5, 22, 39),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            "Your BMI is : ${widget.bmiValue.toStringAsFixed(2)}",
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
