a5b6b46553971dda226a24b591f32747a33c9ff1
rajnish-09 <rajnish9861@gmail.com>
2025-12-11 16:31:27 +0545

    implement conversion logic

--- currency_converter/lib/currency_converter_screen.dart
+++ currency_converter/lib/currency_converter_screen.dart
@@ -12,6 +12,34 @@ class _CurrencyConverterScreenState extends State<CurrencyConverterScreen> {
   TextEditingController inputController = TextEditingController();
   List<String> items = ['NPR', 'INR', 'USD', 'AUD', 'YEN', 'WON'];
   String? selectedValue = 'NPR';
+  String fromValue = 'NPR', toValue = 'NPR';
+
+  Map<String, double> rates = {
+    'NPR': 1,
+    'INR': 1.6,
+    'USD': 133,
+    'AUD': 95,
+    'YEN': 0.9,
+    'WON': 0.10,
+  };
+  double convertedValue = 0;
+
+  void convertCurrency() {
+    if (inputController.text.isEmpty) {
+      setState(() {
+        convertedValue = 0;
+      });
+      return;
+    } else {
+      double amount = double.tryParse(inputController.text) ?? 0;
+      double npr = amount * rates[fromValue]!;
+      double res = npr * rates[toValue]!;
+      setState(() {
+        convertedValue = res;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,30 +63,22 @@ class _CurrencyConverterScreenState extends State<CurrencyConverterScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  SizedBox(
-                    width: 100,
-                    child: DropdownButton<String>(
-                      items: items.map<DropdownMenuItem<String>>((
-                        String value,
-                      ) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      value: selectedValue,
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          selectedValue = newValue;
-                        });
-                      },
-                    ),
+                  Dropdown(
+                    items: items,
+                    selectedValue: fromValue,
+                    onChanged: (value) {
+                      setState(() {
+                        fromValue = value!;
+                      });
+                    },
+                    inputController: inputController,
                   ),
                   SizedBox(
                     width: 200,
                     child: TextField(
                       controller: inputController,
                       decoration: InputDecoration(labelText: 'Enter amount'),
+                      onChanged: (value) => convertCurrency(),
                     ),
                   ),
                 ],
@@ -66,12 +86,69 @@ class _CurrencyConverterScreenState extends State<CurrencyConverterScreen> {
               SizedBox(height: 30),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [Text("Dropdown"), Text("Display")],
+                children: [
+                  Dropdown(
+                    items: items,
+                    selectedValue: toValue,
+                    onChanged: (value) {
+                      setState(() {
+                        toValue = value!;
+                      });
+                      convertCurrency();
+                    },
+                    inputController: inputController,
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: Text(
+                      convertedValue.toStringAsFixed(2),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class Dropdown extends StatefulWidget {
+  final List<String> items;
+  final String selectedValue;
+  final ValueChanged<String?> onChanged;
+  final TextEditingController inputController;
+  const Dropdown({
+    super.key,
+    required this.items,
+    required this.selectedValue,
+    required this.onChanged,
+    required this.inputController,
+  });
+
+  @override
+  State<Dropdown> createState() => _DropdownState();
+}
+
+class _DropdownState extends State<Dropdown> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        SizedBox(
+          width: 100,
+          child: DropdownButton<String>(
+            items: widget.items.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(value: value, child: Text(value));
+            }).toList(),
+            value: widget.selectedValue,
+            onChanged: widget.onChanged,
+          ),
+        ),
+      ],
     );
   }
 }
