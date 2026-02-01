452a4a122730b18a24c3b9267e72ea399bd8e9ba
rajnish-09 <rajnish9861@gmail.com>
2026-02-01 17:51:39 +0545

    feat: payment screen

---
+++ ecommerce_app/lib/models/order_model.dart
@@ -0,0 +1,6 @@
+class OrderMode {
+  final String? id;
+  final String imagePath, title, description;
+  final double price;
+  final double? discount, rating;
+}
--- ecommerce_app/lib/screens/user_module/checkout_screen.dart
+++ ecommerce_app/lib/screens/user_module/checkout_screen.dart
@@ -8,6 +8,7 @@ class CheckoutScreen extends StatefulWidget {
 }
 
 class _CheckoutScreenState extends State<CheckoutScreen> {
+  String selectedValue = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +18,93 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
+              Text("Choose one of the following payment method:"),
+              SizedBox(height: 15),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedValue = 'esewa';
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    border: selectedValue == 'esewa'
+                        ? Border.all(color: Colors.blue)
+                        : Border.all(color: Colors.transparent),
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        "assets/images/esewa.png",
+                        width: 30,
+                        height: 30,
+                      ),
+                      SizedBox(width: 14),
+                      Text("eSewa"),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedValue = 'khalti';
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    border: selectedValue == 'khalti'
+                        ? Border.all(color: Colors.blue)
+                        : Border.all(color: Colors.transparent),
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        "assets/images/khalti.png",
+                        width: 30,
+                        height: 30,
+                      ),
+                      SizedBox(width: 14),
+                      Text("Khalti"),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedValue = 'cod';
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    border: selectedValue == 'cod'
+                        ? Border.all(color: Colors.blue)
+                        : Border.all(color: Colors.transparent),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.attach_money_sharp),
+                      SizedBox(width: 14),
+                      Text("Cash on delivery (Cod)"),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+
+              SizedBox(height: 25),
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
