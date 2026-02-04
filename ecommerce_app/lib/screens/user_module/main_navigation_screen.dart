4e32c4d594af89b9d461191a2af990cd83050fe7
rajnish-09 <rajnish9861@gmail.com>
2026-02-04 17:43:35 +0545

    feat: showing badge for favorites and cart

--- ecommerce_app/lib/screens/user_module/main_navigation_screen.dart
+++ ecommerce_app/lib/screens/user_module/main_navigation_screen.dart
@@ -1,10 +1,17 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:ecommerce_app/bloc/cart/cart_bloc.dart';
+import 'package:ecommerce_app/bloc/cart/cart_event.dart';
+import 'package:ecommerce_app/bloc/cart/cart_state.dart';
+import 'package:ecommerce_app/bloc/favorite/favorite_bloc.dart';
+import 'package:ecommerce_app/bloc/favorite/favorite_event.dart';
+import 'package:ecommerce_app/bloc/favorite/favorite_state.dart';
 import 'package:ecommerce_app/screens/user_module/cart_screen.dart';
 import 'package:ecommerce_app/screens/user_module/favorite_screen.dart';
 import 'package:ecommerce_app/screens/user_module/profile_screen.dart';
 import 'package:ecommerce_app/screens/user_module/shop_screen.dart';
 import 'package:ecommerce_app/widgets/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   final int? currentIndex;
@@ -21,18 +28,14 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   void initState() {
     super.initState();
     currentIndex = widget.currentIndex ?? 0;
+    context.read<FavoriteBloc>().add(LoadFavorites());
+    context.read<CartBloc>().add(FetchCartItems());
   }
 
   final pages = [ShopScreen(), FavoriteScreen(), CartScreen(), ProfileScreen()];
 
   @override
   Widget build(BuildContext context) {
-    final items = [
-      Icon(Icons.home),
-      Icon(Icons.favorite),
-      Icon(Icons.shopping_cart_sharp),
-      Icon(Icons.person),
-    ];
     return Scaffold(
       extendBody: true,
       body: pages[currentIndex],
@@ -40,18 +43,50 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
         data: Theme.of(
           context,
         ).copyWith(iconTheme: IconThemeData(color: Colors.white)),
-        child: CurvedNavigationBar(
-          index: currentIndex,
-          items: items,
-          color: Colors.blue,
-          backgroundColor: Colors.transparent,
-          onTap: (value) {
-            setState(() {
-              currentIndex = value;
-            });
+        child: BlocBuilder<FavoriteBloc, FavoriteState>(
+          builder: (context, state) {
+            int favCount = 0;
+            if (state is FavoriteLoaded) {
+              favCount = state.favorites.length;
+            }
+            return BlocBuilder<CartBloc, CartState>(
+              builder: (context, state) {
+                int cartCount = 0;
+
+                if (state is CartLoaded) {
+                  cartCount = state.cartItems.length;
+                }
+
+                final items = [
+                  Icon(Icons.home),
+                  Badge(
+                    label: Text(favCount.toString()),
+                    isLabelVisible: favCount > 0,
+                    child: Icon(Icons.favorite),
+                  ),
+                  Badge(
+                    label: Text(cartCount.toString()),
+                    isLabelVisible: cartCount > 0,
+                    child: Icon(Icons.shopping_cart_sharp),
+                  ),
+                  Icon(Icons.person),
+                ];
+                return CurvedNavigationBar(
+                  index: currentIndex,
+                  items: items,
+                  color: Colors.blue,
+                  backgroundColor: Colors.transparent,
+                  onTap: (value) {
+                    setState(() {
+                      currentIndex = value;
+                    });
+                  },
+                  animationCurve: Curves.easeInOut,
+                  animationDuration: Duration(milliseconds: 300),
+                );
+              },
+            );
           },
-          animationCurve: Curves.easeInOut,
-          animationDuration: Duration(milliseconds: 300),
         ),
       ),
     );
