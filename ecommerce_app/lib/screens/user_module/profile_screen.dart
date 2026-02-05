65e6fc90de75222e00d31a4caa575bdec2fcdeb0
rajnish-09 <rajnish9861@gmail.com>
2026-02-05 15:45:20 +0545

    feat: profile screen

--- ecommerce_app/lib/screens/user_module/profile_screen.dart
+++ ecommerce_app/lib/screens/user_module/profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce_app/bloc/auth/auth_bloc.dart';
 import 'package:ecommerce_app/bloc/auth/auth_event.dart';
 import 'package:ecommerce_app/bloc/auth/auth_state.dart';
 import 'package:ecommerce_app/screens/login_screen.dart';
+import 'package:ecommerce_app/widgets/submit_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,6 +14,7 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -26,17 +28,104 @@ class _ProfileScreenState extends State<ProfileScreen> {
               );
             }
           },
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  context.read<AuthBloc>().add(LogoutEvent());
-                },
-                child: Text("Logout"),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // ElevatedButton(
+                //   onPressed: () {
+                //     context.read<AuthBloc>().add(LogoutEvent());
+                //   },
+                //   child: Text("Logout"),
+                // ),
+                SizedBox(height: 20),
+                Center(
+                  child: CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage('assets/images/prod1.jpg'),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Text(
+                  "Personal details",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 15),
+                ProfileTextField(controller: emailController, label: 'Name'),
+                SizedBox(height: 15),
+                ProfileTextField(controller: emailController, label: 'Email'),
+                SizedBox(height: 15),
+                ProfileTextField(controller: emailController, label: 'Phone'),
+                SizedBox(height: 30),
+                Text(
+                  "Address details",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 15),
+                ProfileTextField(controller: emailController, label: 'Country'),
+                SizedBox(height: 15),
+                ProfileTextField(
+                  controller: emailController,
+                  label: 'Province',
+                ),
+                SizedBox(height: 15),
+                ProfileTextField(controller: emailController, label: 'City'),
+                SizedBox(height: 15),
+                ProfileTextField(controller: emailController, label: 'Street'),
+                SizedBox(height: 30),
+                SubmitButton(buttonText: 'Save', onPressed: () {}),
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ProfileTextField extends StatelessWidget {
+  const ProfileTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+  });
+
+  final TextEditingController controller;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(label),
+          SizedBox(height: 10),
+          TextFormField(
+            controller: controller,
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide(color: Colors.grey.shade400),
+              ),
+              filled: true,
+              fillColor: Colors.transparent,
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              focusedErrorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
